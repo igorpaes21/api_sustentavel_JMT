28a086198a279e820fc0b7e6cb771481bf953f46
igorpaes21 <irsp2121@hmail.com>
2025-04-17 20:30:08 -0300

    first commit

---
+++ src/main/java/br/com/senai/api_sutentavel/ApiSutentavelApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.api_sutentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSutentavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSutentavelApplication.class, args);
+	}
+
+}
