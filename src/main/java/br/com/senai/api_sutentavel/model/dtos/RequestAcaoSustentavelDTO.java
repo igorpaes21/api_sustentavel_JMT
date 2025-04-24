8bf770c60534e4bf1c6921c4daba60908710bf2d
igorpaes21 <irsp2121@hmail.com>
2025-04-23 21:26:00 -0300

    Inserção de anotações aos métodos para requisições

--- src/main/java/br/com/senai/api_sutentavel/model/dtos/RequestAcaoSustentavelDTO.java
+++ src/main/java/br/com/senai/api_sutentavel/model/dtos/RequestAcaoSustentavelDTO.java
@@ -3,8 +3,11 @@ package br.com.senai.api_sutentavel.model.dtos;
 import br.com.senai.api_sutentavel.model.entity.Usuario;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -15,13 +18,26 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class RequestAcaoSustentavelDTO {
 
-    @NotBlank(message = "Descrição Ação é obrigatório")
+
+    @Getter
+    @NotBlank(message = "O titulo da Ação é obrigatório")
+    @Size(max = 100, message = "Descrição da Ação deve ter no máximo 100 caracteres")
+    private String titulo;
+
+    @NotBlank(message = "Descrição da Ação é obrigatório")
+    @Size(max = 500, message = "Descrição da Ação deve ter no máximo 500 caracteres")
     private String  descricao;
-    @NotNull
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+
+    @NotBlank(message = "A categoria não pode estar em branco")
+    private String categoria;
+
+    @PastOrPresent(message = "A data de realização deve ser bo presente ou no passado")
     private LocalDate dataRealizacao;
-    @NotNull
+
+    @NotBlank(message = "O responsável pela Ação é obrigatório")
     private String responsavel;
-    @NotNull
+
+    @NotNull(message = "O usuário não pode ser nulo")
     private Usuario usuario;
+
 }
--- src/main/java/br/com/senai/api_sutentavel/model/dtos/RequestUsuarioDTO.java
+++ src/main/java/br/com/senai/api_sutentavel/model/dtos/RequestUsuarioDTO.java
@@ -1,6 +1,7 @@
 package br.com.senai.api_sutentavel.model.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +11,31 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RequestUsuarioDTO {
 
+
     @NotBlank(message = "O nome não pode ser vazio")
+    @Size(min = 3, max = 255, message = "O nome deve ter entre 3 e 255 caracteres")
     private String nome;
 
+    @NotBlank(message = "O email não pode ser vazio")
+    @Size(max = 255, message = "O email não pode ter mais de 255 caracteres")
+    private String email;
 
+    @NotBlank(message = "A senha não pode ser vazia")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    private String senha;
 
+    @NotBlank(message = "O CPF não pode ser vazio")
+    private String cpf;
+
+    @NotBlank(message = "O endereço não pode ser vazio")
+    private String endereco;
+
+    @NotBlank(message = "A cidade não pode ser vazia")
+    private String cidade;
+
+    @NotBlank(message = "O estado não pode ser vazio")
+    private String estado;
+
+    private String telefone;
 
 }
