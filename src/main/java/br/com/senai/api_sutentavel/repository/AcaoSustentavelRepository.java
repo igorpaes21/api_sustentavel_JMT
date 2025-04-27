80439a9b281742381f9dd166079c6c9ec331e506
igorpaes21 <irsp2121@hmail.com>
2025-04-27 18:15:48 -0300

    Inserção de método para procurar a ação sustentavél pela categoria.

--- src/main/java/br/com/senai/api_sutentavel/repository/AcaoSustentavelRepository.java
+++ src/main/java/br/com/senai/api_sutentavel/repository/AcaoSustentavelRepository.java
@@ -1,7 +1,11 @@
 package br.com.senai.api_sutentavel.repository;
 
 import br.com.senai.api_sutentavel.model.entity.AcaoSustentavel;
+import br.com.senai.api_sutentavel.model.enums.CategoriaAcao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AcaoSustentavelRepository extends JpaRepository<AcaoSustentavel, Long> {
+    List<AcaoSustentavel> findByCategoria(CategoriaAcao categoria);
 }
